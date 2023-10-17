615ea35ff6a39859ef933cfb25f44eeda51b2eda
Ezzat Mohamed <ezat71101@gmail.com>
2023-10-17 18:34:25 +0300

    refactor init satate & navigate to home

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/Features/Splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,21 +20,13 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-        .animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    
+    navigateToHome();
   }
-
 
   @override
   void dispose() {
-    
     super.dispose();
 
     animationController.dispose();
@@ -50,6 +44,28 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
         SlidingText(slidingAnimation: slidingAnimation),
       ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        GoRouter.of(context).push(AppRouter.kHomeView);
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+//import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const Bookly());
@@ -12,10 +14,10 @@ class Bookly extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
-      home:  const SplashView(),
     );
   }
 }
