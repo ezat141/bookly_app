c889460738b7dae9ee2fcda301d5737e2f02174c
Ezzat Mohamed <ezat71101@gmail.com>
2023-11-25 19:10:00 +0200

    create newest books cubit states

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,23 @@
+part of 'newest_books_cubit.dart';
+
+abstract class NewestBooksState extends Equatable {
+  const NewestBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+class NewestBooksInitial extends NewestBooksState {}
+
+class NewsetBooksLoading extends NewestBooksState {}
+
+class NewsetBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  const NewsetBooksFailure(this.errMessage);
+}
+class NewsetBooksSuccess extends NewestBooksState {
+  final List<BookModel> books;
+
+  const NewsetBooksSuccess(this.books);
+}
+
