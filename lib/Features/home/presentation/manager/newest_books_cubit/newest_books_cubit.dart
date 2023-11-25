210a0e3d5161e9b495705e1743fdc1e061111770
Ezzat Mohamed <ezat71101@gmail.com>
2023-11-25 19:12:09 +0200

    newest books cubit done

--- lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -2,10 +2,21 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../data/models/book_model/book_model.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
   
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
